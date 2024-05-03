80c43ee7e66fe942b60e857cf1bf4276d7832d67
Eric Castillo <113539424+eric-castillo05@users.noreply.github.com>
2024-05-03 04:25:59 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,24 +2,34 @@ from flask import Flask, request, jsonify
 import psycopg2
 import uuid
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
-import psycopg2
 
+@app.route('/signup', methods=['POST'])
+def signup():
+    load_dotenv()
+    data = request.get_json()
+    API_KEY = os.getenv('URL')
+    conn = psycopg2.connect(API_KEY)
+    curr = conn.cursor()
+    curr.execute(
+        "INSERT INTO Usuario (ID_Usuario, Nombre, contrasena, contacto) VALUES (%s, %s, %s, %s)",
+        (data['ID_Usuario'], data['Nombre'], data['contrasena'], data['contacto'])
+    )
+ 
+    conn.commit()
+    conn.close()
+
+    return 'JSON subido a la base de datos con éxito!', 200
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    data = request.get_json()  # Obtén el JSON del cuerpo de la petición POST
-
-    # Conéctate a tu base de datos PostgreSQL
-    conn = psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="sqlDB",
-        user="postgres",
-        password="sharky"
-    )
-    # print(data)
+    load_dotenv()
+    data = request.get_json()
+    API_KEY = os.getenv('URL')
+    conn = psycopg2.connect(API_KEY)
     curr = conn.cursor()
  
     curr.execute(
