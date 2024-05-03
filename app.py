239672f9689e25c5efaa33a87c08302f400ebdd9
Eric Castillo <113539424+eric-castillo05@users.noreply.github.com>
2024-05-03 05:19:58 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -14,15 +14,42 @@ def signup():
     API_KEY = os.getenv('URL')
     conn = psycopg2.connect(API_KEY)
     curr = conn.cursor()
+    
+    curr.execute("SELECT * FROM Usuario WHERE correo = %s", (data['correo'],))
+    if curr.fetchone() is not None:
+        return 'Usuario ya existe', 400
+
+    # Insertar el nuevo usuario en la base de datos
     curr.execute(
-        "INSERT INTO Usuario (ID_Usuario, Nombre, contrasena, contacto) VALUES (%s, %s, %s, %s)",
-        (data['ID_Usuario'], data['Nombre'], data['contrasena'], data['contacto'])
+        "INSERT INTO Usuario (ID_Usuario, Nombre, contrasena, contacto, correo) VALUES (%s, %s, %s, %s, %s)",
+        (data['ID_Usuario'], data['Nombre'], data['contrasena'], data['contacto'], data['correo'])
     )
- 
+    
     conn.commit()
     conn.close()
+    return 'Usuario creado', 201
+    
+ 
+    
+    
 
-    return 'JSON subido a la base de datos con éxito!', 200
+@app.route('/login', methods=['POST'])
+def login():
+    load_dotenv()
+    data = request.get_json()
+    API_KEY = os.getenv('URL')
+    conn = psycopg2.connect(API_KEY)
+    curr = conn.cursor()
+    
+    curr.execute("SELECT * FROM Usuario WHERE correo = %s", (data['correo'],))
+
+    if curr.fetchone() is None:
+        return 'Usuario no valido', 400
+    
+    conn.close()
+    return 'Usuario valido', 201
+    
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
